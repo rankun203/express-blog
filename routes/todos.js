720770e324237227a6e3f299554304b18a779e33
rankun203 <rankun203@gmail.com>
2015-07-02 11:52:56 +0800

    UPDATED: Complete the first todo APIs :-) Go have some 🍹

--- routes/todos.js
+++ routes/todos.js
@@ -3,10 +3,13 @@ var todoService = require('../service/todos');
 
 var router = express.Router();
 
-router
-    .post('/', todoService.createTodo)
-    .get('/', todoService.findTodo)
-    .get('/:id', todoService.findTodo)
-    .delete('/:id', todoService.deleteTodo);
+router.route('/')
+    .post(todoService.createTodo)
+    .get(todoService.findTodo);
+
+router.route('/:id')
+    .get(todoService.findTodo)
+    .delete(todoService.deleteTodo)
+    .put(todoService.updateTodo);
 
 module.exports = router;
--- service/todos.js
+++ service/todos.js
@@ -1,31 +1,41 @@
 var Todo = require('../models/Todo');
 
-module.exports = {
+function cb(res, err, data) {
+    if (err) throw err;
+    else if (data == null)
+        res.status(404).end();
+    else res.json(data);
+}
+
+var TodoService = {
     createTodo: function (req, res, next) {
         Todo.create(req.body, function (err, data) {
-            if (err) res.send(err);
-            else res.json(data);
+            cb(res, err, data);
         });
     },
 
     findTodo: function (req, res, next) {
         if (req.params.id)
             Todo.findById(req.params.id, function (err, data) {
-                if (err) throw err;
-                else res.json(data);
+                cb(res, err, data);
             });
         else
             Todo.find(function (err, data) {
-                if (err) throw err;
-                else res.json(data);
+                cb(res, err, data);
             });
     },
 
     updateTodo: function (req, res, next) {
-
+        Todo.findByIdAndUpdate(req.params.id, req.body, function (err, data) {
+            TodoService.findTodo(req, res);
+        });
     },
 
     deleteTodo: function (req, res, next) {
-
+        Todo.findByIdAndRemove(req.params.id, function (err, data) {
+            cb(res, err, data);
+        });
     }
 };
+
+module.exports = TodoService;
